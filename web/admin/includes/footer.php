ac0e21fcc9a2d0310e5ebc90d2ffed5f982e814a
Junior Cercado Vasquez <juniorcercado@peu.edu.pe>
2021-01-11 22:28:55 -0500

    web

--- web/admin/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-  <div class="pull-right hidden-xs">
-    <b>Version</b> 1.0.0
-  </div>
-  <strong>Copyright &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.issi.pe/" target="_blank">ISSI</a>.</strong> Todos los derechos reservados.
-</footer>
-<!--EUCLIDES DE DISARY SAN LORENSO-->
